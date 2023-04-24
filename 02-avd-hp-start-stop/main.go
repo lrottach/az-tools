aa935f34ddd23c4a90c60e9365b36c15a63a7f4d
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2023-04-24 13:27:11 +0200

    feat: functions to list session hosts of a host pool

--- 02-avd-hp-start-stop/main.go
+++ 02-avd-hp-start-stop/main.go
@@ -11,7 +11,9 @@ import (
 
 func main() {
 
-	subId := ""
+	subId := "e7933ac0-8efa-46fa-9d1e-929ae1dd5e24"
+	rgName := ""
+	hpName := ""
 
 	ctx := context.Background()
 
@@ -30,6 +32,11 @@ func main() {
 		log.Fatalf("error: %v", err)
 	}
 
+	shClient, err := createAzSessionHostsClient(subId, cred)
+	if err != nil {
+		log.Fatalf("failed to acquire new session host client %v: ", err)
+	}
+
 	for _, pool := range hpList {
 		// Print the id of each host pool
 		if pool != nil && pool.ID != nil {
@@ -37,6 +44,17 @@ func main() {
 		}
 	}
 
+	shList, err := listAzSessionHosts(shClient, ctx, rgName, hpName)
+	if err != nil {
+		log.Fatalf("failed query list of session hosts: %v", err)
+	}
+
+	for _, host := range shList {
+		// Print the name of eacht host pools
+		if host != nil && host.ID != nil {
+			fmt.Println("Session Host Id: ", *host.ID)
+		}
+	}
 }
 
 // createAzHostPoolClient takes Azure credentials and generates a new HostPoolsClient
@@ -44,13 +62,23 @@ func createAzHostPoolClient(subId string, cred *azidentity.DefaultAzureCredentia
 
 	client, err := armdesktopvirtualization.NewHostPoolsClient(subId, cred, nil)
 	if err != nil {
-		log.Fatalf("failed to create client: %v", err)
+		log.Fatalf("failed to create HostPoolsClient: %v", err)
 	}
 
 	return client, nil
 }
 
-// listAzHostPoolsfunc craetes a list of
+// createAzSessionHostsClient takes Azure credentials and generates a new SessionHostsClient
+func createAzSessionHostsClient(subId string, cred *azidentity.DefaultAzureCredential) (*armdesktopvirtualization.SessionHostsClient, error) {
+	client, err := armdesktopvirtualization.NewSessionHostsClient(subId, cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create SessionHostsClient: %v", err)
+	}
+
+	return client, nil
+}
+
+// listAzHostPools func creates a list of
 func listAzHostPools(client *armdesktopvirtualization.HostPoolsClient, ctx context.Context) ([]*armdesktopvirtualization.HostPool, error) {
 
 	pager := client.NewListPager(nil)
@@ -59,12 +87,29 @@ func listAzHostPools(client *armdesktopvirtualization.HostPoolsClient, ctx conte
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
 		}
-		for _, v := range nextResult.Value {
-			fmt.Println(v.Name)
-			_ = v
-		}
 		return nextResult.Value, nil
 	}
-	return nil, fmt.Errorf("failed to acquire list of host pools")
 
+	return nil, fmt.Errorf("failed to acquire list of host pools")
+}
+
+// listAzSessionHosts lists all session hosts of a host pool
+func listAzSessionHosts(client *armdesktopvirtualization.SessionHostsClient, ctx context.Context, rgName string, hpName string) ([]*armdesktopvirtualization.SessionHost, error) {
+
+	pager := client.NewListPager(rgName, hpName, nil)
+
+	for pager.More() {
+		nextResult, err := pager.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("failed to advance page: %v", err)
+		}
+
+		for _, v := range nextResult.Value {
+			log.Println("session host %s:", v.Name)
+		}
+
+		return nextResult.Value, nil
+	}
+
+	return nil, fmt.Errorf("failed to acquire list of session hosts")
 }
